44e7fe9562cfe2c4b08853243bb9199bbf139922
Isabel Fernandes Mendoza <Isabel Fernandes Mendoza>
2016-10-12 11:50:35 +0200

    Initial commit

---
+++ Labb1 OOP/Labb1 OOP/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1_OOP
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Breed { get; set; }
+
+        public Dog(string name, int age, string breed)
+        {
+            Name = name;
+            Age = age;
+            Breed = breed;
+
+        }
+    }
+}
