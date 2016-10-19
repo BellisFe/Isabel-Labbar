7b19aff765e8b327c9600615b30e2d80ec9efee7
Isabel Fernandes Mendoza <Isabel Fernandes Mendoza>
2016-10-19 11:32:44 +0200

    test 2

---
+++ Labb2 OOP/Labb2 OOP/Menu.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_OOP
+{
+    class Menu
+    {
+
+        public static void PrintMainMenu()
+        {
+            Runtime runtime = new Runtime();
+            // test
+
+            Console.Clear();
+            Console.WriteLine("1. Add dog");
+            Console.WriteLine("2. Add snake");
+            Console.WriteLine("3. Add crow");
+            Console.WriteLine("4. Remove dog");
+            Console.WriteLine("5. Remove snake");
+            Console.WriteLine("6. Remove crow");
+            Console.WriteLine("7. Show all dogs");
+            Console.WriteLine("8. Show all snakes");
+            Console.WriteLine("9. Show all crows");
+            Console.WriteLine("0. Exit");
+
+            Console.Write("Choice: ");
+            int input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    runtime.addDog();
+                    PrintMainMenu();
+                    break;
+
+                case 2:
+                    runtime.addSnake();
+                    PrintMainMenu();
+                    break;
+
+                case 3:
+                    runtime.addCrow();
+                    PrintMainMenu();
+                    break;
+
+                case 4:
+                    runtime.RemoveDog();
+                    PrintMainMenu();
+                    break;
+
+                case 5:
+                    runtime.RemoveSnake();
+                    PrintMainMenu();
+                    break;
+
+                case 6:
+                    runtime.RemoveCrow();
+                    PrintMainMenu();
+                    break;
+
+                case 7:
+                    foreach (var Item in runtime.allDogs)
+                    {
+                        Console.WriteLine(Item.Information());
+                    }
+                    PrintMainMenu();
+                    break;
+
+                case 8:
+                    foreach (var Item in runtime.allSnakes)
+                    {
+                        Console.WriteLine(Item.Information());
+                    }
+                    PrintMainMenu();
+                    break;
+
+                case 9:
+                    foreach (var Item in runtime.allCrows)
+                    {
+                        Console.WriteLine(Item.Information());
+                    }
+                    PrintMainMenu();
+                    break;
+
+                case 0:
+                    Console.WriteLine("Bye bye");
+                    break;
+            }
+        }
+    }
+}
--- Labb2 OOP/Labb2 OOP/Runtime.cs
+++ Labb2 OOP/Labb2 OOP/Runtime.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.NullReferenceException;
+
 
 namespace Labb2_OOP
 {
     class Runtime
     {
+        public List<Dog> allDogs { get; set; }
+        public List<Snake> allSnakes { get; set; }
+        public List<Crow> allCrows { get; set; }
+
         public void Start()
         {
 
@@ -78,20 +84,162 @@ namespace Labb2_OOP
                 FreeTime = "look for food"
             };
 
-            Console.WriteLine(myDog1.Information());
-            Console.WriteLine(myDog2.Information());
-            Console.WriteLine(mySnake1.Information());
-            Console.WriteLine(mySnake2.Information());
-            Console.WriteLine(myCrow1.Information());
-            Console.WriteLine(myCrow2.Information());
-
-            List<Dog> allDogs = new List<Dog>()
+            
+            allDogs = new List<Dog>()
             {
-                new Dog {Breed = "Yorkie", },
-                new Dog {}
-                
+                myDog1,
+                myDog2
+
             };
 
+            allSnakes = new List<Snake>()
+            {
+                mySnake1,
+                mySnake2
+            };
+
+            allCrows = new List<Crow>()
+            {
+                myCrow1,
+                myCrow2
+            };
+
+            Menu.PrintMainMenu();
+
+        }
+
+
+        public void addDog ()
+        {
+            Dog newDog = new Dog();
+
+            Console.Write("Breed: ");
+            newDog.Breed = Console.ReadLine();
+
+            Console.Write("Sound: ");
+            newDog.Sound = Console.ReadLine();
+
+            Console.Write("Age: ");
+            newDog.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Weight: ");
+            newDog.Weight = Console.ReadLine();
+
+            Console.Write("Way of moving: ");
+            newDog.CanRun = Console.ReadLine();
+
+            Console.Write("Diet: ");
+            newDog.Diet = Console.ReadLine();
+
+            Console.Write("Free time activity: ");
+            newDog.FreeTime = Console.ReadLine();
+
+
+
+            allDogs.Add(newDog);
+
+
+        }
+
+        public void addSnake()
+        {
+            Snake newSnake = new Snake();
+
+            Console.Write("Breed: ");
+            newSnake.Breed = Console.ReadLine();
+
+            Console.Write("Sound: ");
+            newSnake.Sound = Console.ReadLine();
+
+            Console.Write("Age: ");
+            newSnake.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Weight: ");
+            newSnake.Weight = Console.ReadLine();
+
+            Console.Write("Non poisonus or poisonus: ");
+            newSnake.Poisonous = Console.ReadLine();
+
+            Console.Write("Diet: ");
+            newSnake.Diet = Console.ReadLine();
+
+            Console.Write("Free time activity: ");
+            newSnake.FreeTime = Console.ReadLine();
+
+
+
+            allSnakes.Add(newSnake);
+
+
+        }
+
+        public void addCrow()
+        {
+            Crow newCrow = new Crow();
+
+            Console.Write("Breed: ");
+            newCrow.Breed = Console.ReadLine();
+
+            Console.Write("Sound: ");
+            newCrow.Sound = Console.ReadLine();
+
+            Console.Write("Age: ");
+            newCrow.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Weight: ");
+            newCrow.Weight = Console.ReadLine();
+
+            Console.Write("Lenght of wings: ");
+            newCrow.LenghtOfWings = int.Parse(Console.ReadLine());
+
+            Console.Write("Diet: ");
+            newCrow.Diet = Console.ReadLine();
+
+            Console.Write("Free time activity: ");
+            newCrow.FreeTime = Console.ReadLine();
+
+
+            allCrows.Add(newCrow);
+
+        }
+
+        public void RemoveDog()
+        {
+            for (int i = 1; i <= allDogs.Count; i++)
+            {
+                Console.WriteLine( allDogs);
+            }
+
+            Console.Write("Choice: ");
+            int index = int.Parse(Console.ReadLine());
+
+            allDogs.RemoveAt(index - 1);
+        }
+
+        public void RemoveSnake()
+        {
+            for (int i = 1; i <= allSnakes.Count; i++)
+            {
+                Console.WriteLine(allSnakes);
+            }
+
+            Console.Write("Choice: ");
+            int index = int.Parse(Console.ReadLine());
+
+            allSnakes.RemoveAt(index - 1);
+        }
+
+        public void RemoveCrow()
+        {
+            for (int i = 1; i <= allCrows.Count; i++)
+            {
+                Console.WriteLine(allCrows);
+            }
+
+            Console.Write("Choice: ");
+            int index = int.Parse(Console.ReadLine());
+
+            allCrows.RemoveAt(index - 1);
         }
     }
 }
